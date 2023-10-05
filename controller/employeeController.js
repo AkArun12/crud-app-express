8147514a60aa274a3d5aae337046352553fe8019
kathariya arun <kathariya12@gmail.com>
2023-10-05 15:22:12 +0900

    second commit

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,20 +1,43 @@
 
-
+import employeeModel from "../models/Employee.js"
 class EmployeeController{
 
+
+// Create documents
     static createDoc=(req,res)=>{
-        res.redirect("/employee")
-
-    }
-
-    static getAllDoc=(req,res)=>{
-        res.render("index")
+        res.redirect('/employee')
     }
 
 
+
+
+    
+
+// Acccess al documents
+
+    static getAllDoc=async (req,res)=>{
+        try{
+
+            const result=await employeeModel.find()
+       
+             res.render("index", {data:result}) 
+        }
+        catch(err){
+            console.log(err)
+        }
+
+       
+
+    }
+
+   
+// Edit document
     static editDoc=(req,res)=>{
         res.render("edit")
     }
+
+
+// Update document
 
     static updateDocById=(req,res)=>{
          res.redirect("/employee");
--- models/Employee.js
+++ models/Employee.js
@@ -3,5 +3,17 @@ import mongoose from "mongoose";
 
 // defining schema
 
-const studentSchema=new mongoose.Schema()
+const employeeSchema=new mongoose.Schema({
 
+    name:{type:String ,required:true , trim:true},
+    age:{type:Number, required:true ,min:18, max:60},
+    salary:{type:mongoose.Decimal128 ,required:true, validate:(value)=>value>=10000 }
+
+})
+
+
+// Comiling model
+const employeeModel=mongoose.model('employee',employeeSchema)
+
+
+export default employeeModel;
