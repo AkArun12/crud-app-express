c773061cdfb61c13f19a5bf3172b5adea81be0c5
kathariya arun <kathariya12@gmail.com>
2023-10-05 17:43:26 +0900

    Third commit

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ const DATABASE_URL=process.env.DATABASE_URL || "mongodb://localhost:27017";
 // Databse connection 
 connectDB(DATABASE_URL)
 
+// when we want ot get data enterred in form * urlencoded is builtin middleware
+app.use(express.urlencoded({extended:false}))
+
 
 // Static files- publicfolder (we can use css and js now)
 app.use('/employee',express.static(join(process.cwd(),"public")))
--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,55 +1,82 @@
+import employeeModel from "../models/Employee.js";
+class EmployeeController {
+  // Create documents
+  static createDoc = async (req, res) => {
+    // console.log("create doc post method....")
 
-import employeeModel from "../models/Employee.js"
-class EmployeeController{
+    try {
+      const { name, age, salary } = req.body;
 
+      const doc = new employeeModel({
+        name: name,
+        age: age,
+        salary: salary,
+      });
 
-// Create documents
-    static createDoc=(req,res)=>{
-        res.redirect('/employee')
+      //    saving document
+
+      const result = await doc.save();
+      // console.log(result)
+
+      res.redirect("/employee");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Acccess all documents
+
+  static getAllDoc = async (req, res) => {
+    try {
+      const result = await employeeModel.find();
+
+      res.render("index", { data: result });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Edit form document
+  static editDoc = async (req, res) => {
+    // console.log(req.params.id)
+
+    try {
+      const result = await employeeModel.findById(req.params.id);
+      //   console.log(result)
+
+      res.render("edit", { data: result });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Update document
+
+  static updateDocById = async (req, res) => {
+    // console.log(req.params.id)
+    // console.log(req.body)
+    try {
+      const result = await employeeModel.findByIdAndUpdate(
+        req.params.id,
+        req.body
+      );
+    } catch (err) {
+      console.log(err);
     }
 
+    res.redirect("/employee");
+  };
+  
 
-
-
-    
-
-// Acccess al documents
-
-    static getAllDoc=async (req,res)=>{
-        try{
-
-            const result=await employeeModel.find()
-       
-             res.render("index", {data:result}) 
-        }
-        catch(err){
-            console.log(err)
-        }
-
-       
-
+  //   delete document
+  static deleteDocById = async (req, res) => {
+    try {
+      const result = await employeeModel.findByIdAndDelete(req.params.id);
+      res.redirect("/employee");
+    } catch (err) {
+      console.log(err);
     }
-
-   
-// Edit document
-    static editDoc=(req,res)=>{
-        res.render("edit")
-    }
-
-
-// Update document
-
-    static updateDocById=(req,res)=>{
-         res.redirect("/employee");
-    }
-
-    static deleteDocById=(req,res)=>{
-         res.redirect("/employee");
-    }
-
-
+  };
 }
 
-
-export default EmployeeController
-    
+export default EmployeeController;
